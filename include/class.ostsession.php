4f3cc7e994bf566b2bf5fd915151bbb224b45f44
ungsookim <71747708+ungsookim@users.noreply.github.com>
2020-09-24 15:14:24 +0900

    Update class.ostsession.php
    this->data->new SessionData = "";

--- include/class.ostsession.php
+++ include/class.ostsession.php
@@ -186,6 +186,7 @@ extends SessionBackend {
         }
         catch (DoesNotExist $e) {
             $this->data = new SessionData(['session_id' => $id]);
+            $this->data->new SessionData = "";
         }
         catch (OrmException $e) {
             return false;
